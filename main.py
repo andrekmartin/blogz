fc9a3ca90ecc66953e3f1e9248703c7cc1d0d2f7
Andre Martin <andre.k.martin@gmail.com>
2017-10-16 18:57:01 -0500

    Changed connection string & secret key

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:blog-a-build@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:zgolb@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = '383DZU6sPpEOPi'
+app.secret_key = '9Zo5D26xIVw3Kh'
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key = True)
